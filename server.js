73e8b7d6e7346b923d24ee53cb2da8164e45571f
Otis Singfield <singfieldotis@gmail.com>
2021-05-30 16:31:48 -0400

    still fixing things

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-const path = require('path');
 const express = require('express');
-const session = require('express-session');
+const routes = require('./controllers');
+
+const path = require('path');
 const exphbs = require('express-handlebars');
-const helpers = require('./utils/helpers')
+const helpers = require('./utils/helpers');
+
+const session = require('express-session');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -24,14 +27,14 @@ app.use(session(sess));
 
 const hbs = exphbs.create({ helpers });
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(require('./controllers/'));
+app.use(routes)
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
